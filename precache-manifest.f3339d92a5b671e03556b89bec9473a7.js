9a4ed30e11330e5446811f3309c7b6a5ddd9aeed
McGregorSt <stasiak.gregor@gmail.com>
2022-10-03 19:38:57 +0200

    Updates

---
+++ precache-manifest.f3339d92a5b671e03556b89bec9473a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "821442f3144ec9635188",
+    "url": "/new-portfolio/static/css/main.bb6facb0.chunk.css"
+  },
+  {
+    "revision": "821442f3144ec9635188",
+    "url": "/new-portfolio/static/js/main.daed0fa0.chunk.js"
+  },
+  {
+    "revision": "a6640c336988b119c8d8",
+    "url": "/new-portfolio/static/js/runtime~main.cb17adc9.js"
+  },
+  {
+    "revision": "1f43d233868bfb38f310",
+    "url": "/new-portfolio/static/js/2.72dc9277.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/new-portfolio/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "c4689ecb215f4976a106fe012e2f760e",
+    "url": "/new-portfolio/static/media/samuri.c4689ecb.jpg"
+  },
+  {
+    "revision": "fd9f0239160c1982e74c4fafc07bac28",
+    "url": "/new-portfolio/index.html"
+  }
+];
